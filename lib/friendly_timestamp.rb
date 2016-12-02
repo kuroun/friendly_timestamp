b4d84c7242a452e236b2cd804069fbf6e17bc0f0
Kuroun <kuroun.seung@rackspace.com>
2016-12-02 17:53:06 -0600

    Add friendly_timestamp method to DateTime

--- lib/friendly_timestamp.rb
+++ lib/friendly_timestamp.rb
@@ -1,5 +1,39 @@
 require "friendly_timestamp/version"
 
-module FriendlyTimestamp
+#module FriendlyTimestamp
   # Your code goes here...
-end
+  ONE_MINUTE = 60
+  ONE_HOUR = ONE_MINUTE * 60
+  ONE_DAY = ONE_HOUR * 24
+  ONE_WEEK = ONE_DAY * 7
+  ONE_MONTH = ONE_DAY * 30
+  ONE_YEAR = ONE_DAY * 365
+
+  class Time
+
+    def friendly_timestamp
+    # convert time to local time
+    # using minute format if less hour
+    # using hour format if less than a day
+    # using full format otherwise (Thurs, Nov 12th 2016)
+      case (Time.now - self).to_i.abs
+      when 0...ONE_MINUTE
+        'a moment ago'
+      when ONE_MINUTE...ONE_HOUR
+        self.minute_format
+      when ONE_HOUR...ONE_DAY
+        self.hour_format
+      when ONE_DAY...ONE_WEEK
+        self.day_format
+      when ONE_WEEK...ONE_MONTH
+        self.week_format
+      when ONE_MONTH...ONE_YEAR
+        self.month_format
+      when ONE_YEAR...ONE_YEAR * 5
+        self.year_format
+      else
+        self.full_format
+      end
+    end
+  end
+#end
--- spec/friendly_timestamp_spec.rb
+++ spec/friendly_timestamp_spec.rb
@@ -1,11 +1,67 @@
 require "spec_helper"
 
-describe FriendlyTimestamp do
+#describe FriendlyTimestamp::DateTime do
+describe DateTime do
   it "has a version number" do
     expect(FriendlyTimestamp::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#friendly_timestamp' do
+    context 'when less than a minute' do
+      time = Time.now - 30
+      it 'returns string of moment ago' do
+        expect(time.friendly_timestamp).to eq 'a moment ago'
+      end
+    end
+
+    context 'when from 1 minute to less 60 minute' do
+      time = Time.now - 60
+      it 'returns in minute format' do
+        expect(time.friendly_timestamp).to include '1 minute ago'
+      end
+    end
+
+    context 'when from 1 hour to less than 24 hours' do
+      time = Time.now - 60 * 60
+      it 'returns in hour format' do
+        expect(time.friendly_timestamp).to include '1 hour ago'
+      end
+    end
+
+    context 'when from 1 day to less than 7 days' do
+      time = Time.now - 60 * 60 * 24
+      it 'return in day format' do
+        expect(time.friendly_timestamp).to include '1 day ago'
+      end
+    end
+
+    context 'when from 1 week to less than 4 weeks' do
+      time = Time.now - 60 * 60 * 24 * 7
+      it 'returns in week format' do
+        expect(time.friendly_timestamp).to include '1 week ago'
+      end
+    end
+
+    context 'when from 1 month to less than 12 months' do
+      time = Time.now - 60 * 60 * 24 * 30
+      it 'returns in month format' do
+        expect(time.friendly_timestamp).to include '1 month ago'
+      end
+    end
+
+    context 'when from 1 year to less than 10 years' do
+      time = Time.now - 60 * 60 * 24 * 365
+      it 'returns in year format' do
+        expect(time.friendly_timestamp).to include '1 year ago'
+      end
+    end
+
+    context 'when more than or equal 5 years' do
+      time = Time.now - 60 * 60 * 24 * 365 * 5
+      it 'returns full format' do
+        # match regular expression (Thu, Nov 17th 2016 at 17:50)
+        expect(time.friendly_timestamp).to match(/((Sun)|(Mon)|(Tue)|(Wed)|(Thu)|(Fri)|(Sat))(,\s)((Jan)|(Feb)|(Mar)|(Apr)|(May)|(Jun)|(Jul)|(Aug)|(Sep)|(Oct)|(Nov)|(Dec))(\s)[0-9]{1,2}((st)|(nd)|(rd)|(th))(\s)[0-9]{4}(\s)(at)(\s)[0-9]{1,2}(:)[0-9]{1,2}/)
+      end
+    end
   end
 end
